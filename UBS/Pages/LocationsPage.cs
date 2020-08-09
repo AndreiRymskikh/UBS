41885c55b004fda0a3eec741b74b361e7b22460b
andrei_rymskikh <rimskih.a@gmail.com>
2020-08-09 14:07:58 +0200

    redundant folders removed

---
+++ UBS/Pages/LocationsPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace UBS.Pages
+{
+    public class LocationsPage : BasePage
+    {
+        private By Map => By.XPath("//div[@id = 'main']//div[contains(@class, 'map-region')]");
+
+        public LocationsPage(IWebDriver driver) : base(driver) { }
+
+        public bool IsMapDisplayed()
+        {
+            try 
+            {
+                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(Map));
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UBS/StepDefinitions/ContactUsFormSteps.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using UBS.Pages;
+
+namespace UBS.StepDefinitions
+{
+    [Binding, Scope(Feature = "ContactUs form")]
+    public class ContactUsFormSteps : CommonSteps
+    {
+        [Given(@"Domiciles '(.*)' '(.*)' picked")]
+        public void GivenDomicilesOpened(string region, string country)
+        {
+            var homePage = new HomePage(driver);
+            homePage.ClickAgryToAllButton();
+            homePage.SelectDomiciles(region, country);
+        }
+        
+        [Given(@"'(.*)' page displayed")]
+        public void GivenContactUsFormDisplayed(string pageName)
+        {
+            var homePage = new HomePage(driver);
+            homePage.SelectNavMenu(pageName, "Report misconduct of UBS staff");
+            Assert.True(homePage.IsTitleDisplayed(pageName), $"The title '{pageName}' is not displayed");
+        }
+        
+        [When(@"I fill Contact Us form")]
+        public void WhenIFillContactUsForm()
+        {
+            new ContactUsPage(driver).FillContactUsForm();
+        }
+        
+        [Then(@"I can send it to UBS company")]
+        public void ThenICanSendItToUBSCompany()
+        {
+            Assert.IsTrue(new ContactUsPage(driver).ImitateFormSubmit());
+        }
+    }
+}
